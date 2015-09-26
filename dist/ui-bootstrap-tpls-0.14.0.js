aa0b5dcb776ea487733f20ddf4f02c58bcfabc04
Rasul Niyazimbetov <rasulnz@gmail.com>
2015-09-26 10:24:53 +0200

    Revert "UPDATED: made build for new branch"
    This reverts commit 30475a5c5de0280da2e561fc7819ae3463729cdc.

--- dist/ui-bootstrap-tpls-0.14.0.js
+++ dist/ui-bootstrap-tpls-0.14.0.js
@@ -2,7 +2,7 @@
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 0.14.0 - 2015-09-24
+ * Version: 0.14.0 - 2015-09-23
  * License: MIT
  */
 angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.collapse","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.dateparser","ui.bootstrap.position","ui.bootstrap.datepicker","ui.bootstrap.dropdown","ui.bootstrap.stackedMap","ui.bootstrap.modal","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.typeahead"]);
